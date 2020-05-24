87d590a2275e097804b555701331e92d6895a2b5
gabridego <41424282+gabridego@users.noreply.github.com>
2020-05-24 20:42:01 +0200

    Add files via upload

--- daalexnet.py
+++ daalexnet.py
@@ -1,6 +1,10 @@
-import torch
 import torch.nn as nn
-from .utils import load_state_dict_from_url
+from torch.autograd import Function
+
+try:
+    from torch.hub import load_state_dict_from_url
+except ImportError:
+    from torch.utils.model_zoo import load_url as load_state_dict_from_url
 
 
 __all__ = ['AlexNet', 'alexnet']
@@ -10,13 +14,33 @@ model_urls = {
     'alexnet': 'https://download.pytorch.org/models/alexnet-owt-4df8aa71.pth',
 }
 
+''' 
+Very easy template to start for developing your AlexNet with DANN 
+Has not been tested, might contain incompatibilities with most recent versions of PyTorch (you should address this)
+However, the logic is consistent
+'''
 
-class AlexNet(nn.Module):
+class ReverseLayerF(Function):
+    # Forwards identity
+    # Sends backward reversed gradients
+    @staticmethod
+    def forward(ctx, x, alpha):
+        ctx.alpha = alpha
 
+        return x.view_as(x)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        output = grad_output.neg() * ctx.alpha
+
+        return output, None
+
+
+class DAAlexNet(nn.Module):
     def __init__(self, num_classes=1000):
-        super(AlexNet, self).__init__()
+        super(DAAlexNet, self).__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+        	nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
             nn.Conv2d(64, 192, kernel_size=5, padding=2),
@@ -32,7 +56,7 @@ class AlexNet(nn.Module):
         )
         self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
         self.classifier = nn.Sequential(
-            nn.Dropout(),
+        	nn.Dropout(),
             nn.Linear(256 * 6 * 6, 4096),
             nn.ReLU(inplace=True),
             nn.Dropout(),
@@ -40,16 +64,35 @@ class AlexNet(nn.Module):
             nn.ReLU(inplace=True),
             nn.Linear(4096, num_classes),
         )
+        self.dann_classifier = nn.Sequential(
+        	nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, 2),
+        )
 
-    def forward(self, x):
+    def forward(self, x, alpha=None):
         x = self.features(x)
         x = self.avgpool(x)
-        x = torch.flatten(x, 1)
-        x = self.classifier(x)
-        return x
+        # Flatten the features:
+        x = x.view(x.size(0), -1)
+        # If we pass alpha, we can assume we are training the discriminator
+        if alpha is not None:
+            # gradient reversal layer (backward gradients will be reversed)
+            reverse_feature = ReverseLayerF.apply(x, alpha)
+            discriminator_output = self.dann_classifier(reverse_feature)
+            return discriminator_output
+        # If we don't pass alpha, we assume we are training with supervision
+        else:
+            # do something else
+            class_outputs = self.classifier(x)
+            return class_outputs
 
 
-def alexnet(pretrained=False, progress=True, **kwargs):
+def daalexnet(pretrained=False, progress=True, num_classes=1000):
     r"""AlexNet model architecture from the
     `"One weird trick..." <https://arxiv.org/abs/1404.5997>`_ paper.
 
@@ -57,9 +100,19 @@ def alexnet(pretrained=False, progress=True, **kwargs):
         pretrained (bool): If True, returns a model pre-trained on ImageNet
         progress (bool): If True, displays a progress bar of the download to stderr
     """
-    model = AlexNet(**kwargs)
     if pretrained:
-        state_dict = load_state_dict_from_url(model_urls['alexnet'],
-                                              progress=progress)
-        model.load_state_dict(state_dict)
+      model = DAAlexNet()
+
+      state_dict = load_state_dict_from_url(model_urls['alexnet'],
+                                            progress=progress)
+      model.load_state_dict(state_dict, strict=False)
+
+      for l in [1,4]:
+        model.dann_classifier[l].weight.data = model.classifier[l].weight.data
+        model.dann_classifier[l].bias.data = model.classifier[l].bias.data
+
+      model.classifier[6] = nn.Linear(4096, num_classes)
+    else:
+      model = DAAlexNet(num_classes=num_classes)
+    
     return model
